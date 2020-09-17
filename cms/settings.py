5cc48fc2be55e00c6f86f007ed4304fe115deafd
ethan <ethannjw@gmail.com>
2020-09-17 20:47:49 +0800

    set debug value and new requirements.txt

--- cms/settings.py
+++ cms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')4s5@@udx=6cqzukn$wyr38dc07qb+h%p9sntu++4lpwvll50_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = []
 
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'cms.urls'
